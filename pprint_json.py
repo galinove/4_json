7c8cf92a4f7b3c3e6d93395331db288e972df217
Evgeniy Galinov <galinove@gmail.com>
2017-09-23 10:18:14 +0500

    процедуры загрузки и вывода
    параметром получает путь к файлу

--- pprint_json.py
+++ pprint_json.py
@@ -2,12 +2,16 @@ import json
 
 
 def load_data(filepath):
-    pass
+
+    f = open(filepath, encoding="utf8")
+    d = json.loads(f.read())
+    f.close()
+    return d
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, ensure_ascii=False, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    print("this script can't be executed as non-imported")
